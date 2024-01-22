5ab14418057a1d032769873032f86cce246771e3
ErikLerma81 <124100945+ErikLerma81@users.noreply.github.com>
2024-01-21 23:16:32 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,13 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int V1 = 4;
-        int V2 = 8;
 
-        int total = V1 + V2;
-        System.out.printf("Variable one is " + V1 + "\n");
-        System.out.printf("Variable two is " + V2 + "\n");
-        System.out.printf("Total for both is: " + total);
+        enum NBATeam { Jazz, Lakers, Clippers, Blazers }
+
+        NBATeam Erik;
+        Erik = NBATeam.Lakers;
+
+
+
+
+
+        System.out.printf("Your favorite team is the " + Erik);
+
 
     }
 }
