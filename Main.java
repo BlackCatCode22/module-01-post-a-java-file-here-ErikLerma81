3087522bc76dfd11d5d472d07adc3e29ca09e221
ErikLerma81 <124100945+ErikLerma81@users.noreply.github.com>
2024-01-21 23:04:04 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int V1 = 4;
+        int V2 = 8;
+
+        int total = V1 + V2;
+        System.out.printf("Variable one is " + V1 + "\n");
+        System.out.printf("Variable two is " + V2 + "\n");
+        System.out.printf("Total for both is: " + total);
+
+    }
+}
